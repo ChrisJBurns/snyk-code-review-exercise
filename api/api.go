f7f6fecb5b9b97c8de52ffed3fe7b70a90d71d46
ChrisJBurns <29541485+ChrisJBurns@users.noreply.github.com>
2024-10-17 21:09:33 +0100

    adds mutex with map instead of syncmap
    Signed-off-by: ChrisJBurns <29541485+ChrisJBurns@users.noreply.github.com>

--- api/api.go
+++ api/api.go
@@ -35,16 +35,57 @@ type NpmPackageVersion struct {
 	Dependencies map[string]*NpmPackageVersion `json:"dependencies"`
 }
 
+type SafeMap struct {
+	mu sync.RWMutex
+	m  map[string]*NpmPackageVersion
+}
+
+func (sm *SafeMap) Load(key string) (*NpmPackageVersion, bool) {
+	sm.mu.RLock()
+	defer sm.mu.RUnlock()
+	val, ok := sm.m[key]
+	return val, ok
+}
+func (sm *SafeMap) Store(key string, value *NpmPackageVersion) {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	sm.m[key] = value
+}
+
+func (sm *SafeMap) StoreDependency(npmPkg npmPackageResponse, depName string, dep *NpmPackageVersion) {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+
+	key := npmPkg.Name + "-" + npmPkg.Version
+
+	// initialise map if nil
+	if sm.m == nil {
+		sm.m = make(map[string]*NpmPackageVersion)
+	}
+
+	// if package does not exists in map for `key`, add it and it's dependency package.
+	// if package does exist in map for `key`, add it's dependency package
+	if _, ok := sm.m[key]; !ok {
+		sm.m[key] = &NpmPackageVersion{
+			Name:         npmPkg.Name,
+			Version:      npmPkg.Version,
+			Dependencies: make(map[string]*NpmPackageVersion),
+		}
+		sm.m[key].Dependencies[depName] = dep
+	} else {
+		sm.m[key].Dependencies[depName] = dep
+	}
+}
+
 func packageHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	pkgName := vars["package"]
 	pkgVersion := vars["version"]
 
 	rootPkg := &NpmPackageVersion{Name: pkgName, Dependencies: map[string]*NpmPackageVersion{}}
-	// depCache := map[string]*NpmPackageVersion{}
-	var depCacheSync sync.Map
+	var safeMapCache SafeMap
 	var wg sync.WaitGroup
-	if err := fetchAndResolveDependencies(&depCacheSync, rootPkg, pkgVersion, &wg); err != nil {
+	if err := fetchAndResolveDependencies(&safeMapCache, rootPkg, pkgVersion, &wg); err != nil {
 		println(err.Error())
 		w.WriteHeader(500)
 		return
@@ -66,13 +107,12 @@ func packageHandler(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(stringified)
 }
 
-func fetchAndResolveDependencies(depCacheSync *sync.Map, pkg *NpmPackageVersion, versionConstraint string, wg *sync.WaitGroup) error {
-	if cachedDep, ok := depCacheSync.Load(pkg.Name + "-" + versionConstraint); ok {
+func fetchAndResolveDependencies(safeMapCache *SafeMap, pkg *NpmPackageVersion, versionConstraint string, wg *sync.WaitGroup) error {
+	if cachedDep, ok := safeMapCache.Load(pkg.Name + "-" + versionConstraint); ok {
 		fmt.Printf("dep found: %v \n", pkg.Name+"-"+versionConstraint)
-		p := cachedDep.(*NpmPackageVersion)
-		pkg.Version = p.Version
-		pkg.Name = p.Name
-		pkg.Dependencies = p.Dependencies
+		pkg.Version = cachedDep.Version
+		pkg.Name = cachedDep.Name
+		pkg.Dependencies = cachedDep.Dependencies
 		return nil
 	}
 
@@ -82,32 +122,20 @@ func fetchAndResolveDependencies(depCacheSync *sync.Map, pkg *NpmPackageVersion,
 	}
 
 	wg.Add(1)
-	go resolveDependencies(depCacheSync, pkg, npmPkg, wg)
+	go resolveDependencies(safeMapCache, pkg, npmPkg, wg)
 	return nil
 }
 
-func resolveDependencies(depCacheSync *sync.Map, pkg *NpmPackageVersion, npmPkg npmPackageResponse, wg *sync.WaitGroup) error {
+func resolveDependencies(safeMapCache *SafeMap, pkg *NpmPackageVersion, npmPkg npmPackageResponse, wg *sync.WaitGroup) error {
 	defer wg.Done()
-	depCacheSync.Store(npmPkg.Name+"-"+npmPkg.Version, &NpmPackageVersion{
-		Name:         npmPkg.Name,
-		Version:      npmPkg.Version,
-		Dependencies: make(map[string]*NpmPackageVersion),
-	},
-	)
-
-	for dependencyName, dependencyVersionConstraint := range npmPkg.Dependencies {
-		dep := &NpmPackageVersion{Name: dependencyName, Dependencies: map[string]*NpmPackageVersion{}}
-		pkg.Dependencies[dependencyName] = dep
-		if err := fetchAndResolveDependencies(depCacheSync, dep, dependencyVersionConstraint, wg); err != nil {
+	for depName, depVersionConstraint := range npmPkg.Dependencies {
+		dep := &NpmPackageVersion{Name: depName, Dependencies: map[string]*NpmPackageVersion{}}
+		pkg.Dependencies[depName] = dep
+		if err := fetchAndResolveDependencies(safeMapCache, dep, depVersionConstraint, wg); err != nil {
 			return err
 		}
-
-		if depCacheEntry, ok := depCacheSync.Load(npmPkg.Name + "-" + npmPkg.Version); ok {
-			depCacheEntryDependencies := depCacheEntry.(*NpmPackageVersion)
-			depCacheEntryDependencies.Dependencies[dep.Name] = dep
-		}
+		safeMapCache.StoreDependency(npmPkg, dep.Name, dep)
 	}
-
 	return nil
 }
 
